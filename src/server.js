d9d3ad0988aee39add5457d9268597c1998cc4aa
dev7797 <104444555+dev7797@users.noreply.github.com>
2022-05-09 00:17:03 +0545

    change directory

--- src/server.js
+++ src/server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('dist/interview-demo'));
+app.use(express.static('./dist/interview-demo'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/interview-demo/'}),
+    res.sendFile('index.html', {root: './dist/interview-demo/'}),
 );
 
 // Start the app by listening on the default Heroku port
