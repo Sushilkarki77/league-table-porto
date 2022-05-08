d6a9c2185f762655df1216877bf7034cb45ed0cb
dev7797 <104444555+dev7797@users.noreply.github.com>
2022-05-09 00:41:09 +0545

    feat: heroku config"

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
+
+
 app.use(express.static(__dirname + '/dist/interview-demo'));
+
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/interview-demo/index.html'));});
+ 
+res.sendFile(path.join(__dirname+'/dist/interview-demo/index.html'));
+});
+
 app.listen(process.env.PORT || 8080);
