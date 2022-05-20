0cbd049c07dadf0f6067acd672b49b33a42ef4e3
Sushilkarki77 <sushilkarki352@gmail.com>
2022-05-20 20:24:33 +0545

    fix: table interface

--- projects/table/src/lib/table.component.ts
+++ projects/table/src/lib/table.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { TableData } from 'table';
+import { TableData } from './table-interfaces';
 
 @Component({
   selector: 'lib-table',
